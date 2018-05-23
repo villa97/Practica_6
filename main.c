c9f9ee742f4af35debdec2831dc8c1ad0a9c266b
Villa.Cr <30905769+villa97@users.noreply.github.com>
2018-05-22 21:16:19 -0500

    Codigo Base
    Agrego la base para el codigo de la practica.

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <18F4620.h>
+#include <stdlib.h>
+#fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
+#fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
+#use delay(clock=16000000)
+//---Funcion Principal---
+void main (void)
+{
+   while(1)
+   {
+   }
+}
