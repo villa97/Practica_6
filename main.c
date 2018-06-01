66ea104a3019a5f5b0196f9b687078139e652949
JorgeMartinezv <31721524+JorgeMartinezv@users.noreply.github.com>
2018-05-31 20:01:36 -0500

    Variables Globales
    -Variables Principales: Asignaremos las variables que utilizaremos.
    -Arreglos para almacenar los datos del senseo: Asignaremos el espacio y la posición inicial.
    -Arreglo con los valores: Se asigno un espacio de 0 a 9 y los valores representados

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <18F4620.h>
+#fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
+#fuses NOPBADEN, STVREN, NOLVP, NODEBUG,
+#use delay(clock=16000000)
+#use fast_io(A)
+#use fast_io(B)
+#use fast_io(D)
+#use fast_io(E)
+#define dht11 PIN_C4
+#bit inDataDht = 0x94.0
+#byte dataDht = 0x82
+//Banderas
+short procesoCorrecto = 0;
+//Variables Principales.
+unsigned int temperaturaDecena; 
+unsigned int temperaturaUnidad;
+unsigned int humedadDecena;
+unsigned int humedadUnidad;
+//Variable de cambio de senseo
+int LedsTH=0;
+//Arreglos almacenar los datos del senseo
+int valorPosicional[4]={0};
+int digitoMostrar[4]={0};
+//Arreglo con los valores para pintar los numeros de 0 a 9
+int numeros[10]={63,6,91,79,102,109,125,7,127,103};
+void main() 
+{
+   
+}
